dd3d4cab4b68fc4554b5e9e3b3918c7ce63c17b0
Diego Saraiva <diegosaraiva@gmail.com>
2022-11-15 19:41:43 -0300

    refactoring...

---
+++ nvim/lua/user/lsp/settings/clangd.lua
@@ -0,0 +1,48 @@
+local provider = "clangd"
+-- some settings can only passed as commandline flags, see `clangd --help`
+local clangd_flags = {
+    "--background-index",
+    "--fallback-style=Google",
+    "--all-scopes-completion",
+    "--clang-tidy",
+    "--log=error",
+    "--suggest-missing-includes",
+    "--cross-file-rename",
+    "--completion-style=detailed",
+    "--pch-storage=memory", -- could also be disk
+    "--folding-ranges",
+    "--enable-config", -- clangd 11+ supports reading from .clangd configuration file
+    -- NOTE: Workaround for "warning: multiple different client offset_encodings detected for buffer, this is not supported yet".
+    -- Ref: https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428#issuecomment-997226723
+    "--offset-encoding=utf-16", --temporary fix for null-ls
+}
+
+local custom_on_attach = function(client, bufnr)
+    require("lvim.lsp").common_on_attach(client, bufnr)  
+    require("clangd_extensions.inlay_hints").setup_autocmd()
+    require("clangd_extensions.inlay_hints").set_inlay_hints()
+end
+
+
+local custom_on_init = function(client, bufnr)
+    require("lvim.lsp").common_on_init(client, bufnr)
+    require("clangd_extensions.config").setup {}
+    require("clangd_extensions.ast").init()
+    vim.cmd [[
+        command ClangdToggleInlayHints lua require('clangd_extensions.inlay_hints').toggle_inlay_hints()
+        command -range ClangdAST lua require('clangd_extensions.ast').display_ast(<line1>, <line2>)
+        command ClangdTypeHierarchy lua require('clangd_extensions.type_hierarchy').show_hierarchy()
+        command ClangdSymbolInfo lua require('clangd_extensions.symbol_info').show_symbol_info()
+        command -nargs=? -complete=customlist,s:memuse_compl ClangdMemoryUsage lua require('clangd_extensions.memory_usage').show_memory_usage('<args>' == 'expand_preamble')
+    ]]
+end
+
+
+local lsp_opts = {
+    cmd = { provider, unpack(clangd_flags) },
+    on_attach = custom_on_attach,
+    on_init = custom_on_init,
+}
+
+
+return lsp_opts
---
+++ nvim/lua/user/telescope.lua
@@ -0,0 +1,45 @@
+local status_ok, telescope = pcall(require, "telescope")
+if not status_ok then
+    return
+end
+
+local actions = require "telescope.actions"
+
+telescope.setup { 
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        path_display = { "smart" },
+        file_ignore_patterns = { ".git/", "node_modules" },
+
+        mappings = {
+            i = {
+                ["<Down>"] = actions.cycle_history_next,
+                ["<Up>"] = actions.cycle_history_prev,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+            },
+            n = {
+                ["?"] = actions.which_key,
+            },
+        },
+    },
+    pickers = {
+        -- Default configuration for builtin pickers goes here:
+        -- picker_name = {
+        --   picker_config_key = value,
+        --   ...
+        -- }
+        -- Now the picker_config_key will be applied every time you call this
+        -- builtin picker
+    },
+    extensions = {
+        -- Your extension configuration goes here:
+        -- extension_name = {
+            --   extension_config_key = value,
+        -- }
+        -- please take a look at the readme of the extension you want to configure
+    },
+}
