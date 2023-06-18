68baab82871f4c2829776be10cc7dd0f16a194b9
Diego Saraiva <diegosaraiva@gmail.com>
2023-06-18 09:40:42 -0300

    migrating to lazy plugin

--- nvim/lua/user/bufferline.lua
+++ nvim/lua/user/bufferline.lua
@@ -1,138 +1,99 @@
-local status_ok, bufferline = pcall(require, "bufferline")
-if not status_ok then
-    return
-end
-
-bufferline.setup {
-    options = {
-        close_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-        offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
-        separator_style = "slant", -- | "thick" | "thin" | { 'any', 'any' },
-        max_name_length = 30,
-        max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
-        tab_size = 21,
-        diagnostics = false, -- | "nvim_lsp" | "coc",
-        diagnostics_update_in_insert = false,
-        offsets = {
-            {
-                filetype = "undotree",
-                text = "Undotree",
-                highlight = "PanelHeading",
-                padding = 1,
-            },
-            {
-                filetype = "NvimTree",
-                text = "Explorer",
-                highlight = "PanelHeading",
-                padding = 1,
-            },
-            {
-                filetype = "DiffviewFiles",
-                text = "Diff View",
-                highlight = "PanelHeading",
-                padding = 1,
-            },
-            {
-                filetype = "flutterToolsOutline",
-                text = "Flutter Outline",
-                highlight = "PanelHeading",
-            },
-            {
-                filetype = "lazy",
-                text = "Lazy",
-                highlight = "PanelHeading",
-                padding = 1,
-            },
-        },
-    },
-
-    highlights = {
-        fill = {
-            fg = { attribute = "fg", highlight = "#ff0000" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        background = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        buffer_visible = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        close_button = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        close_button_visible = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-    
-        tab_selected = {
-            fg = { attribute = "fg", highlight = "Normal" },
-            bg = { attribute = "bg", highlight = "Normal" },
-        },
-
-        tab = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        tab_close = {
-            fg = { attribute = "fg", highlight = "TabLineSel" },
-            bg = { attribute = "bg", highlight = "Normal" },
-        },
-
-        duplicate_selected = {
-            fg = { attribute = "fg", highlight = "TabLineSel" },
-            bg = { attribute = "bg", highlight = "TabLineSel" },
-            italic = true,
-        },
-
-        duplicate_visible = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-            italic = true,
-        },
-
-        duplicate = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-            italic = true,
-        },
-
-        modified = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        modified_selected = {
-            fg = { attribute = "fg", highlight = "Normal" },
-            bg = { attribute = "bg", highlight = "Normal" },
-        },
-
-        modified_visible = {
-            fg = { attribute = "fg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        separator = {
-            fg = { attribute = "bg", highlight = "TabLine" },
-            bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        separator_selected = {
-            fg = { attribute = "bg", highlight = "Normal" },
-            bg = { attribute = "bg", highlight = "Normal" },
-        },
-
-        indicator_selected = {
-            fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
-            bg = { attribute = "bg", highlight = "Normal" },
+-- A snazzy nail_care buffer line (with tabpage integration) for Neovim built using lua.
+local M = {
+    "akinsho/bufferline.nvim",
+    commit = "c7492a76ce8218e3335f027af44930576b561013",
+    event = { "BufReadPre", "BufAdd", "BufNew", "BufReadPost" },
+    dependencies = {
+        {
+            "famiu/bufdelete.nvim",
+            commit = "8933abc09df6c381d47dc271b1ee5d266541448e",
         },
     },
 }
+
+function M.config()
+    require("bufferline").setup {
+        options = {
+            close_command = "Bdelete! %d",       -- can be a string | function, see "Mouse actions"
+            right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
+            offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+            separator_style = "thin",            -- | "thick" | "thin" | { 'any', 'any' },
+        },
+        highlights = {
+            fill = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            background = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            buffer_visible = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            close_button = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            close_button_visible = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            tab_selected = {
+                fg = { attribute = "fg", highlight = "Normal" },
+                bg = { attribute = "bg", highlight = "Normal" },
+            },
+            tab = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            tab_close = {
+                -- fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'},
+                fg = { attribute = "fg", highlight = "TabLineSel" },
+                bg = { attribute = "bg", highlight = "Normal" },
+            },
+            duplicate_selected = {
+                fg = { attribute = "fg", highlight = "TabLineSel" },
+                bg = { attribute = "bg", highlight = "TabLineSel" },
+                italic = true,
+            },
+            duplicate_visible = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+                italic = true,
+            },
+            duplicate = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+                italic = true,
+            },
+            modified = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            modified_selected = {
+                fg = { attribute = "fg", highlight = "Normal" },
+                bg = { attribute = "bg", highlight = "Normal" },
+            },
+            modified_visible = {
+                fg = { attribute = "fg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            separator = {
+                fg = { attribute = "bg", highlight = "TabLine" },
+                bg = { attribute = "bg", highlight = "TabLine" },
+            },
+            separator_selected = {
+                fg = { attribute = "bg", highlight = "Normal" },
+                bg = { attribute = "bg", highlight = "Normal" },
+            },
+            indicator_selected = {
+                fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
+                bg = { attribute = "bg", highlight = "Normal" },
+            },
+        },
+    }
+end
+  
+return M
--- nvim/lua/user/indentline.lua
+++ nvim/lua/user/indentline.lua
@@ -1,9 +1,11 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
-if not status_ok then
-    return
-end
-
-indent_blankline.setup {
+-- This plugin adds indentation guides to all lines (including empty lines).
+local M = {
+    "lukas-reineke/indent-blankline.nvim",
+    commit = "8299fe7703dfff4b1752aeed271c3b95281a952d",
+    event = "BufReadPre",
+}
+  
+M.opts = {
     char = "▏",
     show_trailing_blankline_indent = false,
     show_first_indent_level = true,
@@ -16,3 +18,5 @@ indent_blankline.setup {
         "NvimTree",
     },
 }
+  
+return M
